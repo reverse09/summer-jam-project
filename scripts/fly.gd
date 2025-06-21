e9859a85b70bf3ca7360798c51987bd6e85b2920
Joseph <joeyklein330@gmail.com>
2025-06-21 00:46:46 -0400

    basic fly

---
+++ scripts/fly.gd
@@ -0,0 +1,28 @@
+extends CharacterBody2D
+
+var speed = 100.0
+var accel = 4
+
+@onready var nav: NavigationAgent2D = $NavigationAgent2D
+
+func _physics_process(delta):
+	var direction = Vector3()
+	var closest_food = null
+	var min_dist = INF
+	var fly_pos = global_position
+
+	for food in get_tree().get_nodes_in_group("food"):
+		var dist = fly_pos.distance_to(food.global_position)
+		if dist < min_dist:
+			min_dist = dist
+			closest_food = food
+
+	nav.target_position = closest_food.global_position
+
+	direction = nav.get_next_path_position() - global_position
+	direction = direction.normalized()
+
+	velocity = velocity.lerp(direction * speed , accel * delta)
+
+	look_at(nav.target_position)
+	move_and_slide()
---
+++ scripts/foodtext.gd
@@ -0,0 +1,11 @@
+extends Node
+
+var rich_text_label: $RichTextLabel
+
+func _ready():
+    SceneTree.create_timer(0.2)
+
+func _on_Timer_timeout():
+    var rng = RandomNumberGenerator.new()
+    rng.randomize()
+    rich_text_label.set_text(char(rng.randi_range(33, 126)))
