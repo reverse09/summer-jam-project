201acb8b78a8f964823ca9bacbe206ec073b33c7
Chr0matic7 <chr0matic5415@gmail.com>
2025-06-21 11:08:14 -0400

    moving scripts to /scripts

--- scripts/foodtext.gd
+++ scripts/foodtext.gd
@@ -3,9 +3,9 @@ extends Node
 var rich_text_label: $RichTextLabel
 
 func _ready():
-    SceneTree.create_timer(0.2)
+	SceneTree.create_timer(0.2)
 
 func _on_Timer_timeout():
-    var rng = RandomNumberGenerator.new()
-    rng.randomize()
-    rich_text_label.set_text(char(rng.randi_range(33, 126)))
+	var rng = RandomNumberGenerator.new()
+	rng.randomize()
+	rich_text_label.set_text(char(rng.randi_range(33, 126)))
