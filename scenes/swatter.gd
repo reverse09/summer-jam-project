c8c1c707e15e5b9cbe29bdccb3c8b75edf736e94
Chr0matic7 <chr0matic5415@gmail.com>
2025-06-20 20:18:17 -0400

    swat animation on click, "swat" input event

--- scenes/swatter.gd
+++ scenes/swatter.gd
@@ -1,7 +1,16 @@
 extends StaticBody2D
 
+@onready var animation_sprite = $AnimatedSprite2D
+
 var mouse_position
 
 func _process(delta):
 	mouse_position = get_global_mouse_position()
 	position = mouse_position
+	
+	if Input.is_action_just_pressed("swat"):
+		animation_sprite.play("swat")
+		
+	
+	
+	
