f5003e27ff4c08539d5904e6fefd10f69ff19b30
Chr0matic7 <chr0matic5415@gmail.com>
2025-06-20 20:27:41 -0400

    swat animation looks better

--- scenes/swatter.gd
+++ scenes/swatter.gd
@@ -4,12 +4,16 @@ extends StaticBody2D
 
 var mouse_position
 
+func swat():
+	animation_sprite.play("swat")
+	
+
 func _process(delta):
 	mouse_position = get_global_mouse_position()
 	position = mouse_position
 	
 	if Input.is_action_just_pressed("swat"):
-		animation_sprite.play("swat")
+		swat()
 		
 	
 	
