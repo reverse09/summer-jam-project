775d79ea6f6a907357a49c3ee6e9735edcf61c43
reverse09 <esrever099@gmail.com>
2025-06-21 20:49:26 -0400

    The stinker

--- scripts/stink_cloud.gd
+++ scripts/stink_cloud.gd
@@ -6,9 +6,13 @@ var hover_frames: float = 0
 @onready var area2d: Area2D = $Area2D
 @onready var destruction_timer: Timer = $DestructionTimer
 @onready var swatter: Swatter = $"../../Swatter"
+@export var cracking : float = 0.0
+@onready var sprite_2d: Sprite2D = $Sprite2D
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 func _process(delta: float) -> void:
 	var overlapping = area2d.get_overlapping_areas()
+	update_sprite()
 	if (overlapping.size() <= 0):
 		hover_frames = 0
 		return
@@ -22,4 +26,7 @@ func _process(delta: float) -> void:
 
 func _on_destruction_timer_timeout() -> void:
 	destruction_timer.stop()
-	self.queue_free()
+	animation_player.play("crack")
+	
+func update_sprite():
+	sprite_2d.material.set_shader_parameter("progress", cracking)
